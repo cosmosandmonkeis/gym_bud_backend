722c0d9262da8e52d9ee78b2c22519a21d1ed413
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-04-08 20:09:46 -0700

    Graphql api definition for preferences and added additional user fields.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,6 +1,15 @@
 const {gql} = require('apollo-server')
 
 module.exports = gql`
+    type Preference {
+        #        Male, Female, or No Preference
+        genderPreference: String
+        #        bodybuilding, calisthenics, pilates, etc... 
+        goalPreference: String
+        #        how many times a week is the user looking to go to the gym
+        frequencyPreference: Int
+    }
+
     type User {
         id: ID
         token: String
@@ -9,7 +18,18 @@ module.exports = gql`
         email: String
         phonenumber: String
         createdAt: String
+        #        did user sign agreement or not
+        signedUserAgreement: Boolean
+        #        what city user is in
+        city: String
+        #        morning, noon, afternoon, evening, night, latenight
+        timeAvailability: String
+        #        example: 24 hour fitness or crunch
+        gymName: String
+        preferences: Preference
     }
+
+
 
     input RegisterInput {
         username: String
@@ -34,6 +54,6 @@ module.exports = gql`
     }
 
     type Subscription {
-#        newBookings: AppointmentBooking
+        #        newBookings: AppointmentBooking
     }
 `
