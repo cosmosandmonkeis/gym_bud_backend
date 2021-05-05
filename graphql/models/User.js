b2c5753aa5148927fcc66e4e68c598a312a418a5
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-05-05 00:52:57 -0700

    Deleted preferences.js resolvers and changing user model's graphql api.

--- graphql/models/User.js
+++ graphql/models/User.js
@@ -1,20 +1,16 @@
 const {model, Schema} = require('mongoose')
 
 const userSchema = new Schema({
+    createdAt: String,
     username: String,
     password: String,
     email: String,
-    createdAt: String,
     admin: Boolean,
     phonenumber: String,
     signedUserAgreement : Boolean,
     city: String,
     timeAvailability: String,
     gymName: String,
-    preferences: {
-        type: Schema.Types.ObjectId,
-        ref: 'preferences'
-    }
 
 })
 
