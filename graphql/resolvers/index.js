cd20fc4a41c35222ea2cb441b4dc00d61371ad4e
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-05-10 21:16:42 -0700

    Added chatResolvers into graphql index.

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,17 @@
 const usersResolvers = require('./users')
+const chatResolvers = require('./chats')
 
 module.exports = {
     Query: {
         ...usersResolvers.Query,
+        ...chatResolvers.Query
     },
     Mutation: {
         ...usersResolvers.Mutation,
+        ...chatResolvers.Mutation
     },
+    Subscription: {
+        ...chatResolvers.Subscription
+    }
 
 }
